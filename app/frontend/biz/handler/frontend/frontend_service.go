32e46984a33afa29f92b45dce0c8bbb207232195
AdrianWang <wyz17601402786@gmail.com>
2025-01-24 14:08:21 +0800

    初始化前端模块，添加基础配置、路由和数据库连接逻辑，生成相关代码文件

---
+++ app/frontend/biz/handler/frontend/frontend_service.go
@@ -0,0 +1,9 @@
+package frontend
+
+import (
+	"context"
+
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/protocol/consts"
+	"github.com/douyin-shop/douyin-shop/app/frontend/biz/utils"
+)
---
+++ app/frontend/go.mod
@@ -0,0 +1,3 @@
+module github.com/douyin-shop/douyin-shop/app/frontend
+
+replace github.com/apache/thrift => github.com/apache/thrift v0.13.0
