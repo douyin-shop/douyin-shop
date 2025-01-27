3525e708731504928381951103abe891a4b98b4f
AdrianWang <wyz17601402786@gmail.com>
2025-01-27 18:10:16 +0800

    优化响应格式，统一错误和成功响应的结构

--- app/frontend/biz/utils/resp.go
+++ app/frontend/biz/utils/resp.go
@@ -8,12 +8,22 @@ import (
 
 // SendErrResponse  pack error response
 func SendErrResponse(ctx context.Context, c *app.RequestContext, code int, err error) {
-	// todo edit custom code
-	c.String(code, err.Error())
+
+	var resp = map[string]interface{}{
+		"code": -1,
+		"data": nil,
+		"msg":  err.Error(),
+	}
+	c.JSON(code, resp)
 }
 
 // SendSuccessResponse  pack success response
 func SendSuccessResponse(ctx context.Context, c *app.RequestContext, code int, data interface{}) {
-	// todo edit custom code
-	c.JSON(code, data)
+
+	var resp = map[string]interface{}{
+		"code": 0,
+		"data": data,
+		"msg":  "success",
+	}
+	c.JSON(code, resp)
 }
