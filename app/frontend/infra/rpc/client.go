bc305c5d08ee4e998e9643a814790e99c6be90e8
AdrianWang <wyz17601402786@gmail.com>
2025-01-30 14:20:03 +0800

    新增RPC客户端初始化，集成OpenTelemetry追踪功能

--- app/frontend/infra/rpc/client.go
+++ app/frontend/infra/rpc/client.go
@@ -1,2 +1,34 @@
 // @Author Adrian.Wang 2025/1/30 11:24:00
 package rpc
+
+import (
+	"github.com/cloudwego/kitex/client"
+	"github.com/cloudwego/kitex/pkg/klog"
+	"github.com/cloudwego/kitex/pkg/rpcinfo"
+	"github.com/douyin-shop/douyin-shop/app/auth/kitex_gen/auth/authservice"
+	"github.com/douyin-shop/douyin-shop/app/frontend/conf"
+	"github.com/douyin-shop/douyin-shop/app/user/kitex_gen/user/userservice"
+	"github.com/douyin-shop/douyin-shop/common/nacos"
+	kitextracing "github.com/kitex-contrib/obs-opentelemetry/tracing"
+)
+
+var (
+	UserClient userservice.Client
+	AuthClient authservice.Client
+)
+
+func InitClient() {
+	// 通过微服务调用user服务
+	resolver := nacos.GetNacosResolver()
+	klog.Info("初始化rpc client")
+	UserClient = userservice.MustNewClient("user",
+		client.WithResolver(resolver),
+		client.WithSuite(kitextracing.NewClientSuite()),
+		client.WithClientBasicInfo(&rpcinfo.EndpointBasicInfo{ServiceName: conf.GetConf().Hertz.Service}),
+	)
+	AuthClient = authservice.MustNewClient("auth",
+		client.WithResolver(resolver),
+		client.WithSuite(kitextracing.NewClientSuite()),
+		client.WithClientBasicInfo(&rpcinfo.EndpointBasicInfo{ServiceName: conf.GetConf().Hertz.Service}),
+	)
+}
