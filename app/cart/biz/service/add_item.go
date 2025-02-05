7e5a1dd689bd55ab23713931d17b33c63f6f06dd
AdrianWang <wyz17601402786@gmail.com>
2025-02-05 12:12:40 +0800

    实现购物车添加商品功能，集成商品服务校验并新增购物车模型方法

--- app/cart/biz/service/add_item.go
+++ app/cart/biz/service/add_item.go
@@ -2,7 +2,13 @@ package service
 
 import (
 	"context"
+	"github.com/cloudwego/kitex/pkg/kerrors"
+	"github.com/douyin-shop/douyin-shop/app/cart/biz/dal/model"
+	"github.com/douyin-shop/douyin-shop/app/cart/biz/dal/mysql"
+	"github.com/douyin-shop/douyin-shop/app/cart/biz/utils/code"
 	cart "github.com/douyin-shop/douyin-shop/app/cart/kitex_gen/cart"
+	"github.com/douyin-shop/douyin-shop/app/cart/rpc"
+	"github.com/douyin-shop/douyin-shop/app/product/kitex_gen/product"
 )
 
 type AddItemService struct {
@@ -14,7 +20,25 @@ func NewAddItemService(ctx context.Context) *AddItemService {
 
 // Run create note info
 func (s *AddItemService) Run(req *cart.AddItemReq) (resp *cart.AddItemResp, err error) {
-	// Finish your business logic.
+	// 调用Product服务校验商品是否存在
+	productResp, err := rpc.ProductClient.GetProduct(s.ctx, &product.GetProductReq{Id: req.Item.ProductId})
+	if err != nil {
+		return nil, err
+	}
 
-	return
+	if productResp == nil || productResp.Product.Id == 0 {
+		return nil, code.GetError(code.NotFoundProduct)
+	}
+
+	cartItem := &model.Cart{
+		UserId:    int32(req.UserId),
+		ProductId: int32(req.Item.ProductId),
+		Quantity:  req.Item.Quantity,
+	}
+
+	err = model.AddItem(s.ctx, mysql.DB, cartItem)
+	if err != nil {
+		return nil, kerrors.NewBizStatusError(500, err.Error())
+	}
+	return &cart.AddItemResp{}, nil
 }
