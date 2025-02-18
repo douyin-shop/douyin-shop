9174b28fe9ad93ac2083532e39fc8665a47b69e6
AdrianWang <wyz17601402786@gmail.com>
2025-02-18 13:01:27 +0800

    优化订单列表服务，增加日志记录和请求响应的复制功能

--- app/frontend/biz/service/list_order.go
+++ app/frontend/biz/service/list_order.go
@@ -2,9 +2,13 @@ package service
 
 import (
 	"context"
+	"github.com/cloudwego/hertz/pkg/common/hlog"
+	"github.com/douyin-shop/douyin-shop/app/checkout/rpc"
+	"github.com/jinzhu/copier"
 
 	"github.com/cloudwego/hertz/pkg/app"
 	order "github.com/douyin-shop/douyin-shop/app/frontend/hertz_gen/frontend/order"
+	remote_order "github.com/douyin-shop/douyin-shop/app/order/kitex_gen/order"
 )
 
 type ListOrderService struct {
@@ -17,10 +21,27 @@ func NewListOrderService(Context context.Context, RequestContext *app.RequestCon
 }
 
 func (h *ListOrderService) Run(req *order.ListOrderReq) (resp *order.ListOrderResp, err error) {
-	//defer func() {
-	// hlog.CtxInfof(h.Context, "req = %+v", req)
-	// hlog.CtxInfof(h.Context, "resp = %+v", resp)
-	//}()
-	// todo edit your code
+	defer func() {
+		hlog.CtxInfof(h.Context, "req = %+v", req)
+		hlog.CtxInfof(h.Context, "resp = %+v", resp)
+	}()
+
+	remoteListOrderReq := &remote_order.ListOrderReq{}
+	err = copier.Copy(remoteListOrderReq, req)
+	if err != nil {
+		return nil, err
+	}
+
+	remoteOrderResp, err := rpc.OrderClient.ListOrder(h.Context, remoteListOrderReq)
+
+	if err != nil {
+		return nil, err
+	}
+
+	resp = &order.ListOrderResp{}
+	err = copier.Copy(resp, remoteOrderResp)
+	if err != nil {
+		return nil, err
+	}
 	return
 }
