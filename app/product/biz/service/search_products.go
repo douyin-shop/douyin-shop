74ffeee40dee40771139a1a98a2edded86ee0fbf
AdrianWang <wyz17601402786@gmail.com>
2025-02-24 15:48:46 +0800

    修改了一下日志

--- app/product/biz/service/search_products.go
+++ app/product/biz/service/search_products.go
@@ -2,8 +2,8 @@ package service
 
 import (
 	"context"
+	"github.com/cloudwego/kitex/pkg/klog"
 
-	"github.com/cloudwego/kitex/pkg/kerrors"
 	"github.com/douyin-shop/douyin-shop/app/product/biz/code"
 	"github.com/douyin-shop/douyin-shop/app/product/biz/dal/es"
 	"github.com/douyin-shop/douyin-shop/app/product/biz/dal/es/document"
@@ -18,34 +18,49 @@ func NewSearchProductsService(ctx context.Context) *SearchProductsService {
 	return &SearchProductsService{ctx: ctx}
 }
 
-// Run create note info
-
-/**
-这里搜索商品基于ES的定义,一共有两种搜索方式,模糊搜索和精确搜索,涉及参数为：
-Keyword     string 
-PageNum     int     
-PageSize    int    
-CategoryName string 
-MinPrice    float64 
-MaxPrice    float64
-这里我们通过KeyWord和CategoryName来确定搜索方式,如果KeyWord不为空,
-则进行模糊搜索,如果CategoryName不为空,则进行精确搜索,如果两个都不为空,
-则进行混合搜索,混合搜索的规则为:先进行模糊搜索,再对模糊搜索的结果进行过滤,
-**/
-
+// Run 这里搜索商品基于ES的定义,一共有两种搜索方式,模糊搜索和精确搜索,涉及参数为：
+//
+// # Keyword     string
+//
+// # PageNum     int
+//
+// # PageSize    int
+//
+// # CategoryName string
+//
+// # MinPrice    float64
+//
+// # MaxPrice    float64
+// 这里我们通过KeyWord和CategoryName来确定搜索方式,如果KeyWord不为空,
+// 则进行模糊搜索,如果CategoryName不为空,则进行精确搜索,如果两个都不为空,
+// 则进行混合搜索,混合搜索的规则为:先进行模糊搜索,再对模糊搜索的结果进行过滤,
 func (s *SearchProductsService) Run(req *product.SearchProductsReq) (resp *product.SearchProductsResp, err error) {
-	pros,c:=document.CombinedSearchProduct(es.Client, conf.GetConf().ElasticSearch.IndexName,
-	req.SearchQuery.KeyWord, req.SearchQuery.CategoryName, 
-	req.SearchQuery.MinPrice, req.SearchQuery.MaxPrice, 
-	int(req.SearchQuery.PageNum),int(req.SearchQuery.PageSize))
-	if c==code.Error{
-		return nil,kerrors.NewGRPCBizStatusError(int32(c),code.GetMessage(c))
+
+	klog.Debugf("SearchProductsService Run req:%+v", req)
+
+	// 设置搜索条件，如果SearchQuery为nil，则初始化一个
+	if req.SearchQuery == nil {
+		req.SearchQuery = &product.SearchQuery{}
+	}
+
+	esProducts, err := document.CombinedSearchProduct(es.Client,
+		conf.GetConf().ElasticSearch.IndexName,
+		req.SearchQuery.KeyWord,
+		req.SearchQuery.CategoryName,
+		req.SearchQuery.MinPrice,
+		req.SearchQuery.MaxPrice,
+		int(req.SearchQuery.PageNum),
+		int(req.SearchQuery.PageSize),
+	)
+	if err != nil {
+		klog.Error(s.ctx, "SearchProductsService Run error:%v", err)
+		return nil, code.GetErr(code.ESSearchError)
 	}
 	var products []*product.Product
-	for _,product:=range pros{
-		products=append(products,PtoM1(product))
+	for _, product := range esProducts {
+		products = append(products, PtoM1(product))
 	}
 	return &product.SearchProductsResp{
-		Results:products,
+		Results: products,
 	}, nil
 }
