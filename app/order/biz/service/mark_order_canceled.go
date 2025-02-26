c7e194565e72fa8e7917ac794859ee2ddff9258c
AdrianWang <wyz17601402786@gmail.com>
2025-02-26 13:23:22 +0800

    新增订单取消功能，获取订单对应的购物车商品并通知购物车服务加回商品

--- app/order/biz/service/mark_order_canceled.go
+++ app/order/biz/service/mark_order_canceled.go
@@ -3,9 +3,12 @@ package service
 import (
 	"context"
 	"github.com/cloudwego/kitex/pkg/klog"
+	"github.com/douyin-shop/douyin-shop/app/cart/kitex_gen/cart"
 	"github.com/douyin-shop/douyin-shop/app/order/biz/dal/model"
 	"github.com/douyin-shop/douyin-shop/app/order/biz/dal/mysql"
 	order "github.com/douyin-shop/douyin-shop/app/order/kitex_gen/order"
+	"github.com/douyin-shop/douyin-shop/app/order/rpc"
+	"github.com/douyin-shop/douyin-shop/app/order/utils/code"
 )
 
 type MarkOrderCanceledService struct {
@@ -24,6 +27,28 @@ func (s *MarkOrderCanceledService) Run(req *order.MarkOrderCanceledReq) (resp *o
 	if err != nil {
 		klog.Error("mark order canceled failed", err)
 		return nil, err
+	}
+
+	// 获取订单对应的购物车商品
+	orderDetail, err := model.GetOrderByID(mysql.DB, orderId)
+	if err != nil {
+		klog.Error("get order by id failed", err)
+		return nil, code.GetError(code.GetOrderError)
+	}
+
+	// 通知购物车服务加回购物车商品
+	for _, orderItem := range orderDetail.OrderItems {
+		_, err := rpc.CartClient.AddItem(s.ctx, &cart.AddItemReq{
+			UserId: orderDetail.UserID,
+			Item: &cart.CartItem{
+				ProductId: orderItem.ProductID,
+				Quantity:  orderItem.Quantity,
+			},
+		})
+		if err != nil {
+			klog.Error("add item to cart failed", err)
+			return nil, code.GetError(code.RecoversError)
+		}
 	}
 
 	resp = &order.MarkOrderCanceledResp{}
